3e9e7328bef3531a8fbf5377d71600e2a572897f
Ay-ob <ayoubhadjyoucef1@gmail.com>
2024-09-23 01:11:32 +0100

    ✨ feat:  add  images and create horizontal scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import { useScroll, motion, useTransform } from "framer-motion";
+import { useRef } from "react";
+import { ReactLenis } from "lenis/react";
+import images from "./assets/images";
 function App() {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["10%", "-600%"]);
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </>
+    <ReactLenis root>
+      <section
+        ref={targetRef}
+        className='relative min-h-[1000vh] bg-neutral-900'
+      >
+        <div className='sticky top-0 flex h-screen items-center overflow-hidden'>
+          <motion.div style={{ x }} className='flex items-center gap-10'>
+            {images.map((img, index) => (
+              <img
+                key={index}
+                src={img}
+                alt={index}
+                loading='lazy'
+                className='h-[calc(100vh-200px)] object-cover rounded-3xl
+              '
+              />
+            ))}
+          </motion.div>
+        </div>
+      </section>
+    </ReactLenis>
   );
 }
 
---
+++ src/assets/images/index.js
@@ -0,0 +1,41 @@
+import img1 from "./adrar.jpg";
+import img2 from "./batna.jpg";
+import img3 from "./image1.jpg";
+import img4 from "./image2.jpg";
+import img5 from "./image3.jpg";
+import img6 from "./pexels-deykhd-11463236.jpg";
+import img7 from "./pexels-henrik-le-botos-1588507-3878114.jpg";
+import img8 from "./pexels-louai-benzaoui-435435842-17067405.jpg";
+import img9 from "./pexels-mohamedelaminemsiouri-28291077.jpg";
+import img10 from "./pexels-mohamedelaminemsiouri-4601737.jpg";
+import img11 from "./pexels-mohammed-alouani-2947035-15199828.jpg";
+import img12 from "./pexels-mohammed-alouani-2947035-15321994.jpg";
+import img13 from "./pexels-mohammed-alouani-2947035-21847351.jpg";
+import img14 from "./pexels-nabil-nait-abderrahmane-1193364286-21998526.jpg";
+import img15 from "./pexels-noureddine-14912071.jpg";
+import img16 from "./pexels-noureddine-belfethi-1160352-9866975.jpg";
+import img17 from "./pexels-noureddine-mechden-256420164-13044503.jpg";
+import img18 from "./pexels-ramzi-belaidi-527587592-19720676.jpg";
+import img19 from "./pexels-umbra-bandits-1851611-16171448.jpg";
+
+export default [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+];
